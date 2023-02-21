a011065ce1f4854ca16908a8aded2bfefea6d119
Vito <sandrinvito10@gmail.com>
2023-02-21 01:21:52 -0300

    fix: structure

--- server/src/routes/projects.ts
+++ server/src/routes/projects.ts
@@ -5,15 +5,15 @@ import Service from "../services/projects";
 const router = Router();
 const service = new Service();
 
-router.post("/:id/task/remove", service.removeTask);
+
 router.patch("/:id/task/set-completed", service.setCompleted);
-
-router.post("/:id/add-user", service.addUsertoProject);
-router.post("/:id/remove-user", service.removeUserFromProject);
-
+router.post("/:id/task/remove", service.removeTask);
 router.get("/:id/task/:task", service.findOneTask);
 router.patch("/:id/task/:task", service.updateTask);
 router.post("/:id/task", service.newTask);
+
+router.post("/:id/add-user", service.addUsertoProject);
+router.post("/:id/emove-user", service.removeUserFromProject);
 
 router.post("/remove", service.remove);
 router.get("/:id", service.findOne);
--- server/src/services/projects.ts
+++ server/src/services/projects.ts
@@ -131,62 +131,6 @@ class Projects {
     }
   };
 
-  addUsertoProject = async (req: Request, res: Response) => {
-    const { body, params } = req;
-
-    const user = await this.user.findOne(req, { email: body.user });
-    const project = await this.project.findOne(req, { _id: params.id });
-    if (!user) {
-      response(res, 404, "User not found!");
-      return;
-    }
-    if (!project) {
-      response(res, 404, "Project not found!");
-      return;
-    }
-
-    try {
-      await this.project.update(
-        req,
-        { _id: params.id },
-        { $push: { user: user?._id } }
-      );
-      response(res, 200, `${user?.name} added to project ${project?.name}`);
-    } catch (error) {
-      console.log(error);
-      response(res, 502);
-    }
-  };
-
-  removeUserFromProject = async (req: Request, res: Response) => {
-    const { params, body } = req;
-
-    const project = await this.project.findOne(req, { _id: params.id });
-    const user = await this.user.findOne(req, { email: body.user });
-
-    if (!project) {
-      response(res, 404, "Project not found!");
-      return;
-    }
-
-    try {
-      await this.project.update(
-        req,
-        { _id: params.id },
-        { $pull: { user: user?._id } }
-      );
-
-      response(
-        res,
-        200,
-        `User ${user?.name} removed from project ${project.name}`
-      );
-    } catch (error) {
-      console.log(error);
-      response(res, 502);
-    }
-  };
-
   newTask = async (req: Request, res: Response) => {
     const { body, params } = req;
 
@@ -274,6 +218,7 @@ class Projects {
       }
       response(res, 200, `${body.tasks.length} tasks excluded successfully!`);
     } catch (error) {
+      console.log(error);
       response(res, 502);
     }
   };
@@ -297,6 +242,62 @@ class Projects {
       }
       response(res, 200, `${body.tasks.length} tasks updated successfully!`);
     } catch (error) {
+      response(res, 502);
+    }
+  };
+
+  addUsertoProject = async (req: Request, res: Response) => {
+    const { body, params } = req;
+
+    const user = await this.user.findOne(req, { email: body.user });
+    const project = await this.project.findOne(req, { _id: params.id });
+    if (!user) {
+      response(res, 404, "User not found!");
+      return;
+    }
+    if (!project) {
+      response(res, 404, "Project not found!");
+      return;
+    }
+
+    try {
+      await this.project.update(
+        req,
+        { _id: params.id },
+        { $push: { user: user?._id } }
+      );
+      response(res, 200, `${user?.name} added to project ${project?.name}`);
+    } catch (error) {
+      console.log(error);
+      response(res, 502);
+    }
+  };
+
+  removeUserFromProject = async (req: Request, res: Response) => {
+    const { params, body } = req;
+
+    const project = await this.project.findOne(req, { _id: params.id });
+    const user = await this.user.findOne(req, { email: body.user });
+
+    if (!project) {
+      response(res, 404, "Project not found!");
+      return;
+    }
+
+    try {
+      await this.project.update(
+        req,
+        { _id: params.id },
+        { $pull: { user: user?._id } }
+      );
+
+      response(
+        res,
+        200,
+        `User ${user?.name} removed from project ${project.name}`
+      );
+    } catch (error) {
+      console.log(error);
       response(res, 502);
     }
   };
