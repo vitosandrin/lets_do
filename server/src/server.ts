9f53ddd2dd8896853268703908e39cb54643a7b9
Vito <sandrinvito10@gmail.com>
2023-02-24 19:06:15 -0300

    feat WebSocket function

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,24 +1,17 @@
 import dotenv from "dotenv";
 import http from "http";
-import { Server, Socket } from "socket.io";
 import express from "./config/express";
-import { handleSocketConnection } from "./ws";
+import { ServerSocket } from "./ws";
 dotenv.config();
 
 const { APP_PORT, APP_HOST } = process.env;
 
 const app = http.createServer(express);
-export const io = new Server(app, {
-  cors: {
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST"],
-  },
-});
-
-handleSocketConnection(io);
 
 const server = app.listen(APP_PORT, (): void =>
   console.log(`Server Running at http://${APP_HOST}:${APP_PORT}`)
 );
 
-export default server;
+new ServerSocket(server);
+
+export { server };
--- server/src/ws.ts
+++ server/src/ws.ts
@@ -1,14 +1,62 @@
-import { Server, Socket } from "socket.io";
-import webSocketRoutes from "./routes/webSocket";
+import { Server as HttpServer } from "http";
+import { Socket, Server } from "socket.io";
+import { ChatModel } from "./models/chats";
+import { UserModel } from "./models/users";
+import Service from "./services/service";
 
-export const handleSocketConnection = (io: Server) => {
-  io.on("connection", (socket: Socket) => {
-    console.log(`User Connected: ${socket.id}`);
+export class ServerSocket {
+  public static instance: ServerSocket;
+  public io: Server;
+  public chat;
+  constructor(server: HttpServer) {
+    ServerSocket.instance = this;
+    const chat = Service(ChatModel);
+    this.chat = chat;
 
-    webSocketRoutes(io, socket);
+    this.io = new Server(server, {
+      pingInterval: 10000,
+      pingTimeout: 5000,
+      cors: {
+        origin: "*",
+        methods: ["GET", "POST"],
+      },
+    });
+
+    this.io.on("connect", this.connect);
+  }
+
+  connect = (socket: Socket) => {
+    console.info("Connection received from ", socket.id);
+
+    socket.on("joinProject", (data) => {
+      socket.join(data);
+    });
+
+    socket.on("sendChat", async (req, data) => {
+      const chatData = {
+        project: data.project,
+        user: data.user,
+        content: data.content,
+        createdAt: Date.now(),
+      };
+
+      const newChat = await this.chat.create(req, chatData);
+
+      try {
+        const lastChat = await this.chat.findAll(
+          req,
+          { limit: 1 },
+          { _id: newChat?._id }
+        );
+
+        socket.to(data.project).emit("receiveChat", lastChat.result[0]);
+      } catch (error) {
+        console.log(error);
+      }
+    });
 
     socket.on("disconnect", () => {
-      console.log("User Disconnected", socket.id);
+      console.info("Disconnect received from: ", socket.id);
     });
-  });
-};
+  };
+}
